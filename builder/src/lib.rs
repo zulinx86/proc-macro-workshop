f618060291d30a488cae2d9b0cc579c06d7704ee
Takahiro Itazuri <itazur@amazon.com>
2023-02-25 18:27:41 +0000

    builder: parse input and return empty TokenStream

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    TokenStream::new()
 }
