f2341b59faf53274ebb216a6e7b1fdd8e359be0e
Takahiro Itazuri <itazur@amazon.com>
2023-02-25 23:00:26 +0000

    builder(refactor): split into multiple functions

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,34 +1,39 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident, Type, Visibility};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    match generate_builder(input) {
+    match generate(input) {
         Ok(generated) => generated,
         Err(err) => err.to_compile_error().into(),
     }
 }
 
-fn generate_builder(derive_input: DeriveInput) -> Result<TokenStream, syn::Error> {
+struct ParseResult {
+    ident: Ident,
+    vis: Visibility,
+    fields: Vec<(Ident, Type)>,
+}
+
+fn parse(derive_input: DeriveInput) -> Result<ParseResult, syn::Error> {
+    // Parse input
     let ident = derive_input.ident;
     let vis = derive_input.vis;
-    let builder_ident = format_ident!("{}Builder", ident);
-
-    let (fields_ident, fields_ty): (Vec<_>, Vec<_>) = match derive_input.data {
+    let fields: Vec<(Ident, Type)> = match derive_input.data {
         Data::Struct(data) => match data.fields {
             Fields::Named(fields) => fields
                 .named
                 .into_iter()
                 .map(|field| {
-                    // SAFETY: Named fields.
+                    // SAFETY: `fields` is a `FieldsNamed` object.
                     let ident = field.ident.unwrap();
                     let ty = field.ty;
                     (ident, ty)
                 })
-                .unzip(),
+                .collect(),
             _ => {
                 return Err(syn::Error::new_spanned(
                     ident,
@@ -44,7 +49,51 @@ fn generate_builder(derive_input: DeriveInput) -> Result<TokenStream, syn::Error
         }
     };
 
-    let checks = fields_ident.iter().map(|ident| {
+    Ok(ParseResult { ident, vis, fields })
+}
+
+fn apply_fields<F>(fields: &[(Ident, Type)], f: F) -> Vec<TokenStream>
+where
+    F: Fn(&(Ident, Type)) -> TokenStream,
+{
+    fields.iter().map(f).collect()
+}
+
+fn builder_declare(
+    builder_ident: &Ident,
+    vis: &Visibility,
+    fields: &[(Ident, Type)],
+) -> TokenStream {
+    let builder_fields = apply_fields(fields, |(ident, ty)| {
+        quote! {
+            #ident: Option<#ty>
+        }
+    });
+
+    quote! {
+        #vis struct #builder_ident {
+            #(#builder_fields),*
+        }
+    }
+}
+
+fn builder_setters(fields: &[(Ident, Type)]) -> TokenStream {
+    let setters = apply_fields(fields, |(ident, ty)| {
+        quote! {
+            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    });
+
+    quote! {
+        #(#setters)*
+    }
+}
+
+fn builder_build_checkers(fields: &[(Ident, Type)]) -> TokenStream {
+    let checkers = apply_fields(fields, |(ident, _)| {
         let err_msg = format!("{} is required.", ident);
         quote! {
             if self.#ident.is_none() {
@@ -53,34 +102,74 @@ fn generate_builder(derive_input: DeriveInput) -> Result<TokenStream, syn::Error
         }
     });
 
-    let expanded = quote! {
-        #vis struct #builder_ident {
-            #(#fields_ident: Option<#fields_ty>),*
+    quote! {
+        #(#checkers)*
+    }
+}
+
+fn builder_build_ret(ident: &Ident, fields: &[(Ident, Type)]) -> TokenStream {
+    let items = apply_fields(fields, |(ident, _)| {
+        quote! {
+            #ident: self.#ident.clone().unwrap()
         }
+    });
 
-        impl #builder_ident {
-            #(pub fn #fields_ident(&mut self, #fields_ident: #fields_ty) -> &mut Self {
-                self.#fields_ident = Some(#fields_ident);
-                self
-            })*
+    quote! {
+        Ok(#ident {
+            #(#items),*
+        })
+    }
+}
 
-            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
-                #(#checks)*
+fn builder_build(ident: &Ident, fields: &[(Ident, Type)]) -> TokenStream {
+    let builder_build_checkers = builder_build_checkers(fields);
+    let builder_build_ret = builder_build_ret(ident, fields);
 
-                Ok(#ident {
-                    #(#fields_ident: self.#fields_ident.clone().unwrap()),*
-                })
-            }
+    quote! {
+        pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+            #builder_build_checkers
+            #builder_build_ret
         }
+    }
+}
 
+fn init_builder(ident: &Ident, builder_ident: &Ident, fields: &[(Ident, Type)]) -> TokenStream {
+    let items = apply_fields(fields, |(ident, _)| {
+        quote! {
+            #ident: None
+        }
+    });
+
+    quote! {
         impl #ident {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
-                    #(#fields_ident: None),*
+                    #(#items),*
                 }
             }
         }
+    }
+}
+
+fn generate(derive_input: DeriveInput) -> Result<proc_macro::TokenStream, syn::Error> {
+    let ParseResult { ident, vis, fields } = parse(derive_input)?;
+
+    let builder_ident = format_ident!("{}Builder", ident);
+    let builder_declare = builder_declare(&builder_ident, &vis, &fields);
+    let builder_setters = builder_setters(&fields);
+    let builder_build = builder_build(&ident, &fields);
+    let init_builder = init_builder(&ident, &builder_ident, &fields);
+
+    let expanded = quote! {
+        #builder_declare
+
+        impl #builder_ident {
+            #builder_setters
+            #builder_build
+        }
+
+        #init_builder
     };
 
-    Ok(TokenStream::from(expanded))
+    Ok(proc_macro::TokenStream::from(expanded))
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,24 @@
 // To run the code:
 //     $ cargo run
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .current_dir("..".to_owned())
+        .build()
+        .unwrap();
+
+    assert_eq!(command.executable, "cargo");
+}
